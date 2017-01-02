f55419a4986a676c0c51d48a14dc15b3a80dbc62
Ain <ain.salula@gmail.coms>
2017-01-03 01:13:01 +0200

    url as user input argument

--- words.py
+++ words.py
@@ -1,8 +1,9 @@
+import sys
 from urllib.request import urlopen
 
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt') as story:
+def fetch_words(url):
+    with urlopen(url) as story:
         story_words = []
         for line in story:
             line_words = line.decode('utf-8').split()
@@ -11,14 +12,14 @@ def fetch_words():
     return story_words
 
 
-def print_words(story_words):
-    for word in story_words:
-        print(word)
+def print_items(items):
+    for item in items:
+        print(item)
 
 
-def main():
-    words = fetch_words()
-    print_words(words)
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
