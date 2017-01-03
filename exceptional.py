f3c142185df8e7169e0ba5a84c9e386445cc254c
Ain <ain.salula@gmail.coms>
2017-01-03 18:27:58 +0200

    std err

--- exceptional.py
+++ exceptional.py
@@ -1,12 +1,17 @@
 '''A module for demonstrating exceptions.'''
 
 
+import sys
+
+
 def convert(s):
     '''Convert to an integer.'''
     try:
         x = int(s)
         print("Conversion succeeded! x=", x)
-    except (ValueError, TypeError):
-        print("conversion failed!")
+    except (ValueError, TypeError) as e:
+        print("Conversion error: {}"
+              .format(str(e)),
+              file=sys.stderr)
         x = -1
     return x
